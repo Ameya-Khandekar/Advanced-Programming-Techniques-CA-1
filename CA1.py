bee9e587018598e418546f5042154c12f0e615f4
Ameya Khandekar <79254284+Ameya-Khandekar@users.noreply.github.com>
2022-11-25 11:43:54 +0000

    Add files via upload

---
+++ CA1.py
@@ -0,0 +1,7 @@
+class Employee:
+    def __init__(self, name, weeklyhours, rate, overtimeRate, weeklytaxcredit):
+        self.name = name
+        self.weeklyhours = weeklyhours
+        self.rate = rate
+        self.overtimeRate = overtimeRate
+        self.weeklytaxcredit = weeklytaxcredit
