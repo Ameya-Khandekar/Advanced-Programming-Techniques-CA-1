edda72f3d5702f20e68f348167437f073713e125
Ameya Khandekar <79254284+Ameya-Khandekar@users.noreply.github.com>
2023-02-15 13:01:53 +0000

    Update tax calculator.py

--- tax calculator.py
+++ tax calculator.py
@@ -1,1 +1,20 @@
 # Employee Tax Calculator
+class Employee:
+    def __init__(self, name, weeklyhours, rate, overtimeRate, weeklytaxcredit):
+        self.name = name
+        self.weeklyhours = weeklyhours
+        self.rate = rate
+        self.overtimeRate = overtimeRate
+        self.weeklytaxcredit = weeklytaxcredit
+
+    def computeWeeklyPay(self, hours):
+        if hours <= self.weeklyhours:
+            return hours * self.rate
+        else:
+            regular_pay = self.weeklyhours * self.rate
+            overtime_pay = (hours - self.weeklyhours) * self.overtimeRate
+            return regular_pay + overtime_pay
+
+    def computeTax(self, grossPay):
+        tax = max(0, 0.4 * grossPay - self.weeklytaxcredit)
+        return tax
